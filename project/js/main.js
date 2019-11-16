0e149a3ac84cf166009c3eeffe76e4f3a30c2ec3
Ironman <robinneuman@hotmail.com>
2019-11-16 14:18:06 +0100

    added more styling and also added second language

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(e){
-    
+    $(".viewBtn").click(function(e) {
+        let target = $(this).parent().parent().find("tr");
+            if(target.attr("class") == "d-none"){
+                target.removeClass("d-none");
+            }else{
+                target.addClass("d-none")
+            }
+    });
+
+    $("#svenska").click(function(e){
+        $("body").find("[lang='en']").addClass("d-none");
+        $("body").find("[lang='sv']").removeClass("d-none");
+    })
+    $("#english").click("english", function(e){
+        $("body").find("[lang='sv']").addClass("d-none");
+        $("body").find("[lang='en']").removeClass("d-none");
+    })    
 })
+
