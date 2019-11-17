52fc891ea90decdc2bf7896715c15c4717fbb628
Ironman <robinneuman@hotmail.com>
2019-11-17 22:47:31 +0100

    Finished hide all and view all buttons

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,18 @@
 $(document).ready(function(e){
     $(".viewBtn").click(function(e) {
-        let target = $(this).parent().parent().find("tr");
-            if(target.attr("class") == "d-none"){
-                target.removeClass("d-none");
-            }else{
-                target.addClass("d-none")
-            }
+        let target = $(this).parent().parent().parent().find("tr");
+        let btnTargetEn = $(this).parent().find("[lang='en']");
+        let btnTargetSv = $(this).parent().find("[lang='sv']");
+        if(target.attr("class") == "d-none"){
+            target.removeClass("d-none");
+            $(btnTargetEn).text("hide");           
+            $(btnTargetSv).text("dölj");           
+            
+        }else{
+            target.addClass("d-none")
+            $(btnTargetEn).text("view");           
+            $(btnTargetSv).text("visa"); 
+        }           
     });
 
     $("#svenska").click(function(e){
@@ -20,6 +27,34 @@ $(document).ready(function(e){
     $(".projectsNav").click(function(e){
         let content = document.getElementById("projects");
         content.scrollIntoView({behavior: "smooth"});
-})
+    })
+    
+    $("#viewAll").click(function(e) {
+        let target = $("#personSkillset").find("tr");
+        let btnTargetEn = $(".headerBtn").find("[lang='en']");
+        let btnTargetSv = $(".headerBtn").find("[lang='sv']");
+            target.removeClass("d-none");       
+            
+            if(target.attr("class") != "d-none"){
+                target.removeClass("d-none");
+                $(btnTargetEn).text("hide");           
+                $(btnTargetSv).text("dölj");           
+                
+            }
+    });
+
+    $("#hideAll").click(function(e) {
+        let target = $("#personSkillset").find("tr");
+        let btnTargetEn = $(".headerBtn").find("[lang='en']");
+        let btnTargetSv = $(".headerBtn").find("[lang='sv']");
+        target.addClass("d-none")
+        
+        if(target.attr("class") == "d-none"){
+            target.addClass("d-none");
+            $(btnTargetEn).text("view");           
+            $(btnTargetSv).text("visa");           
+            
+        }
+    });
 })
 
